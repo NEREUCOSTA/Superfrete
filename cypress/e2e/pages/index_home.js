7dc44ee85e1d2dad3fbfe6fbbaeb6284bb143074
nereu.nogueira <nereu.nogueira@gsoftware.com.br>
2024-09-07 17:38:21 -0300

    projeto SuperFrete

--- cypress/e2e/pages/index_home.js
+++ cypress/e2e/pages/index_home.js
@@ -1,9 +1,43 @@
 class HomePage{
 
     validarHomePage(){
-        cy.get('.sc-qZrbh > img').contains(' SuperFrete')
+        cy.get('[data-cy="calculator-submit"]').should('be.visible')
 
     }
-  
+
+    preencherAltura(alt){
+    cy.get('#weight').click()
+    cy.get('[data-value="0.3"]').click()
+    cy.get('#packageHeight').click() // digitar a Altura
+    cy.get('#packageHeight').type(alt)
+
+    }
+
+    preecherLargura(larg){
+        cy.get('#packageWidth').click() // digitar a largura
+        cy.get('#packageWidth').type(larg)
+    }
+
+    preencherComprimento(compri){
+        cy.get('#packageDepth').click() // digitar o comprimento
+        cy.get('#packageDepth').type(compri)
+    }
+    
+    cepOrigem(cepO){
+        cy.get('#originPostcode').click() // digitar o cep de origem
+        cy.get('#originPostcode').type(cepO)
+    }
+    cepDestino(cepD){
+        cy.get('#destinationPostcode').click() // digitar o CEP destino
+        cy.get('#destinationPostcode').type(cepD)
+    }
+
+    botaoSubmit(){
+        cy.get('[data-cy="calculator-submit"]').click()
+    }
+    
+    validarNegativa(msm){
+        cy.contains(msm).should('be.visible')
+    }
    
 } export default new HomePage;
--- cypress/e2e/test_cases/home_page.cy.js
+++ cypress/e2e/test_cases/home_page.cy.js
@@ -1,34 +1,99 @@
-import login from "../pages/index_home"
+import pages from "../pages/index_home";
 
-describe.only('Validações de Comprimento - SuperFrete', () => {
+describe('Validações de Comprimento - SuperFrete', () => {
 
-    beforeEach(() => {
-        cy.visit('https://web.superfrete.com/');
-        login.validarHomePage()
-        cy.on('uncaught:exception', (err, runnable) => {
-        // Adicione lógica para tratar erros específicos ou ignorar erros
-        if (err.message.includes('User is not logged!')) {
-          return false; // Ignorar este erro específico
-        }
-        return true; // Permitir que outros erros façam o teste falhar
-      
-      });
-      
+  beforeEach(() => {
+    // Definir a resolução padrão da tela
+    cy.viewport(1366, 768);
+    cy.clearCookies();
+    cy.clearLocalStorage()
+    cy.visit('https://web.superfrete.com/');
+    cy.wait(10000)
+    pages.validarHomePage();
+    // Tratamento de exceções específico
+     cy.on('uncaught:exception', (err) => {
+        return false;
     });
+  });
+
+  it('selecionar o peso 300 gramas e efetuar o cálculo do frete', () => {
+    
+    pages.preencherAltura(2)
+    pages.preecherLargura(11)
+    pages.preencherComprimento(16)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
      
-      
-    });
-  
-    it('Deve exibir erro para comprimento menor que 13 cm', () => {
-      cy.get('input[name="length"]').type('12');
-      cy.get('button[type="submit"]').click();
-      cy.contains('Comprimento inválido').should('be.visible');
-    });
-  
-    it('Deve exibir erro para comprimento maior que 150 cm', () => {
-      cy.get('input[name="length"]').type('151');
-      cy.get('button[type="submit"]').click();
-      cy.contains('Comprimento inválido').should('be.visible');
-    });
-  
-  
+  });
+
+  it('Deve exibir erro para Altura menor do 0.4', () => {
+    pages.preencherAltura(0.3)
+    pages.preecherLargura(11)
+    pages.preencherComprimento(16)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
+    pages.validarNegativa('Altura mínima 0.4 cm')
+   
+  });
+
+  it('Deve exibir erro para Altura maior que 150 cm', () => {
+    pages.preencherAltura(153)
+    pages.preecherLargura(11)
+    pages.preencherComprimento(16)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
+    pages.validarNegativa('Comprimento máximo 150 cm.')
+    
+  });
+
+  it('Deve exibir erro para Largura menor do que 8cm', () => {
+    pages.preencherAltura(120)
+    pages.preecherLargura(6)
+    pages.preencherComprimento(16)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
+    pages.validarNegativa('Largura mínima 8 cm.')
+    
+  });
+
+  it('Deve exibir erro para Largura maior do que 150cm', () => {
+    pages.preencherAltura(120)
+    pages.preecherLargura(156)
+    pages.preencherComprimento(16)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
+    pages.validarNegativa('Largura máxima 150 cm.')
+    
+  });
+
+  it.only('Deve exibir erro para comprimento menor do que 13cm', () => {
+    pages.preencherAltura(120)
+    pages.preecherLargura(11)
+    pages.preencherComprimento(11)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
+    pages.validarNegativa('Comprimento mínimo 13 cm.')
+    
+  });
+
+  it('Deve exibir erro para comprimento maior do que 150cm', () => {
+    pages.preencherAltura(120)
+    pages.preecherLargura(11)
+    pages.preencherComprimento(160)
+    pages.cepOrigem('71926500')
+    pages.cepDestino('08090-284')
+    pages.botaoSubmit()
+    pages.validarNegativa('Comprimento máximo 150 cm.')
+    
+  });
+
+
+}); // fim do Describe
+
+// observação - report Bug - ao preencher os campos com valores minimos informados no CAMPO ALTURA, ainda assim aparece o campo para digitar o email, o certo seria exibir a altura com o erro  e o sistema não ter continuidade.
